6e035bfd00c382b5c750736687f94c45cd09ea25
Mohamed ZOGHLAMI <ma.zoghlami@hotmail.fr>
2020-08-27 13:32:10 +0200

    Initialisation système mail (+identifiants smtp)

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,16 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\ContactMail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
     public function insert(Request $request){
-            return [
-                'firstname' => $request->firstname,
-                'lastname' => $request->lastname,
-                'email' => $request->email,
-                'message' => $request->message
-            ];
+        return [
+            'firstname' => $request->firstname,
+            'lastname' => $request->lastname,
+            'email' => $request->email,
+            'message' => $request->message
+        ];
     }
 }
