b164a559d827911a7213b10bf469ac915dc457da
Mohamed ZOGHLAMI <ma.zoghlami@hotmail.fr>
2020-08-27 16:59:03 +0200

    Ajout de l'url api POST create-checkout-session #30

---
+++ app/Http/Controllers/StripeController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Stripe\Stripe;
+
+class StripeController extends Controller
+{
+    protected function checkout(Request $request){
+        Stripe::setApiKey(config('stripe.publishable_key'));
+        $session = \Stripe\Checkout\Session::create([
+            'payment_method_types' => ['card'],
+            'line_items' => [[
+                'price_data' => [
+                    'currency' => 'eur',
+                    'product_data' => [
+                        'name' => 'T-shirt',
+                    ],
+                    'unit_amount' => $request->amount,
+                ],
+                'quantity' => 1,
+            ]],
+            'mode' => 'payment',
+            'success_url' => 'http://localhost:8000/success',
+            'cancel_url' => 'http://localhost:8000/cancel',
+        ]);
+        return response(['id' => $session->id], 200);
+    }
+}
