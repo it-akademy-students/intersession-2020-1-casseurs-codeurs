f36db02feacb0066bccdd5807379f7a4e1e2b8d7
jakysardoo <s.poiree@it-students.fr>
2020-08-27 11:20:20 +0200

    add api authentication routes

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,25 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('github/{username}/{repos}', 'ApiController@github');
 
 Route::post('contact', 'ContactController@insert');
+
+// routes authentification
+Route::prefix('v1')->group(function () {
+    Route::prefix('auth')->group(function () {
+        // Below mention routes are public, user can access those without any restriction.
+        // Create New User
+        Route::post('register', 'AuthController@register');
+        // Login User
+        Route::post('login', 'AuthController@login');
+        
+        // Refresh the JWT Token
+        Route::get('refresh', 'AuthController@refresh');
+        
+        // Below mention routes are available only for the authenticated users.
+        Route::middleware('auth:api')->group(function () {
+            // Get user info
+            Route::get('user', 'AuthController@user');
+            // Logout user from application
+            Route::post('logout', 'AuthController@logout');
+        });
+    });
+});
