d3b9ab7b66190eed103c974d8dc677903c38caa8
jakysardoo <s.poiree@it-students.fr>
2020-09-02 17:29:22 +0200

    Add reset password user function

--- routes/api.php
+++ routes/api.php
@@ -31,7 +31,7 @@ Route::prefix('auth')->group(function () {
     // Login User
     Route::post('login', 'AuthController@login');
     // Send reset password mail
-    Route::post('reset-password', 'AuthController@sendPasswordResetLink');
+    Route::post('reset-password/', 'AuthController@sendPasswordResetLink');
     // handle reset password form process
     Route::post('reset/password', 'AuthController@callResetPassword');
     
