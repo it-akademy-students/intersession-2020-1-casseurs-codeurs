18193ac5ac0e02bae890f528f3f5b69e65285312
jakysardoo <s.poiree@it-students.fr>
2020-09-04 10:03:10 +0200

    Correction bug reset mail sur MailResetPasswordNotification.php

--- app/Notifications/MailResetPasswordNotification.php
+++ app/Notifications/MailResetPasswordNotification.php
@@ -45,7 +45,11 @@ class MailResetPasswordNotification extends ResetPassword
         return ( new MailMessage )
             ->from('it.intersession.swapp@gmail.com', 'SWAPP - Security Scan for Web Apps')
             ->subject( 'Reset Password Notification' )
-            ->markdown('emails.reset')->with('link', $link);
+            //->markdown('emails.reset')->with('link', $link)
+            ->line( "Hello! You are receiving this email because we received a password reset request for your account." )
+            ->action( 'Reset Password', $link )
+            ->line( "This password reset link will expire in ".config('auth.passwords.users.expire')." minutes" )
+            ->line( "If you did not request a password reset, no further action is required." );
     }
 
     /**
